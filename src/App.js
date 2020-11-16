2707344fbbb18c89c4e09787dd3db3a1d11cd638
Rafael 99 <chipalino2009@list.ru>
2020-11-16 12:47:27 +0300

    Added Router and Create Opt-for(input) _netflix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, SignIn, SignUp, Browse } from './pages/';
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/signin">
+        <SignIn />
+      </Route>
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+    </Router>
   );
 }
 
