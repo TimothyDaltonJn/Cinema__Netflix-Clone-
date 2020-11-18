d143480f7f9e70362c2eb27ca012b07486bff430
Rafael 99 <chipalino2009@list.ru>
2020-11-18 20:29:31 +0300

    Added Spinner review

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, SignIn, SignUp, Browse } from './pages/';
+import { IsUserRedirect, PotectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
         <SignIn />
-      </Route>
-      <Route exact path="/signup">
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
         <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <PotectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </PotectedRoute>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
         <Home />
-      </Route>
+      </IsUserRedirect>
     </Router>
   );
 }
